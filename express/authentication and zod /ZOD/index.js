fba871570dd6f067ea8b0a9c87cf9269e21a5e59
Anshul Nema <156672450+Anshul2308z@users.noreply.github.com>
2025-04-21 13:44:29 +0530

    Add files via upload

--- express/authentication and zod /ZOD/index.js
+++ express/authentication and zod /ZOD/index.js
@@ -1,32 +1,86 @@
-// debugging required : endpoint 20 april/22:37
+
+// // valid email and password creation
+// // expecting input as an object with str username and pwd
+
+// const express = require("express");
+// const app = express();
+// const z = require("zod");
+
+
+// app.use(express.json());
+
+// app.post("/", (req, res) => {
+//     const obj = req.body;
+//     const schema = z.object({
+//         username: z.string().email(),
+//         password: z.string().min(8).max(20),
+//     })
+//     const response = schema.safeParse(obj);
+//     if (!response.success) {
+//         res.status(400).json({
+//             message: "Invalid input",
+//             errors: response.error.errors,
+//         })
+//         console.log("failed");
+//     }
+//     else {
+//         res.send({
+//             response
+//         });
+//         console.log(response.success);
+//     }
+
+
+// })
 
 
 
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// }
+// );
+// Its working, I was just about to cry :(
 
-const express = require(express);
-const z = require(zod);
+/*IMP 
+ in json 
+[{},{}] : array of obj 
+ {} : object
 
+*/
+
+
+const express = require("express");
 const app = express();
+const z = require("zod"); // zod documentation at https://zod.dev/?id=basic-usage
 
-const schema = z.array(z.number()); 
 
+
+const schema = z.array(z.number());
 app.use(express.json());
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
     const kidneys = req.body.kidneys;
     const response = schema.safeParse(kidneys)
+
+    if (!response.success) {
+        res.status(400).json({
+            message: "Invalid input",
+            errors: response.error.errors,// this will give you the error message
+            
+        })
+        console.log("failed");
+    }
+    else{
     res.send({
         response
-    })
-
-    res.json({
-        message: `Received ${kidneys.length} kidneys`,
-        kidneys,
     });
+    console.log(response.success);
+}
 
-});
-
-app.use(function(err,req,res,next){
+}
+)
+//global catches
+app.use(function(err, req, res, next) {
     res.status(500).json({
         msg: "Yikes ,internal server error",
     });
@@ -34,7 +88,25 @@ app.use(function(err,req,res,next){
 })
 
 
-
 app.listen(3000, () => {    
-    console.log("Server is running on port 3000");
-})
+    console.log("Server is running on port 3000");})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
